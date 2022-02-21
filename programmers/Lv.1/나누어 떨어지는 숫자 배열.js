5aaf1fa312b706b41a836e973c0c5bf0e2f0023a
Corete95 <fanthell95@gmail.com>
2022-02-21 19:05:06 +0900

    2022.02.21 Lv1 문제풀이

---
+++ programmers/Lv.1/나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,21 @@
+/*
+
+array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
+divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
+
+입출력 예#1
+arr의 원소 중 5로 나누어 떨어지는 원소는 5와 10입니다. 따라서 [5, 10]을 리턴합니다.
+
+입출력 예#2
+arr의 모든 원소는 1으로 나누어 떨어집니다. 원소를 오름차순으로 정렬해 [1, 2, 3, 36]을 리턴합니다.
+
+입출력 예#3
+3, 2, 6은 10으로 나누어 떨어지지 않습니다. 나누어 떨어지는 원소가 없으므로 [-1]을 리턴합니다.
+
+*/
+
+function solution(arr, divisor) {
+  let newArr = arr.filter((v) => v % divisor === 0).sort((a, b) => a - b);
+  let result = newArr.length ? newArr : [-1];
+  return result;
+}
