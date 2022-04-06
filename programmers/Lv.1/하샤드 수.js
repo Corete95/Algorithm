a394ec654e2d33634ad6d3b125441b07b52d696f
Corete95 <fanthell95@gmail.com>
2022-04-06 21:19:12 +0900

    [Update]README 2022.04.06 업데이트

---
+++ programmers/Lv.1/하샤드 수.js
@@ -0,0 +1,22 @@
+/*
+
+양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다. 
+예를 들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수입니다. 
+자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수, solution을 완성해주세요.
+
+ex)  arr = 10  return = true
+     10의 모든 자릿수의 합은 1입니다. 10은 1로 나누어 떨어지므로 10은 하샤드 수입니다.
+
+     arr = 11  return = false
+     11의 모든 자릿수의 합은 2입니다. 11은 2로 나누어 떨어지지 않으므로 11는 하샤드 수가 아닙니다.
+
+*/
+
+function solution(x) {
+  let arr = x.toString().split("");
+  let number = 0;
+  for (let i = 0; i < arr.length; i++) {
+    number += Number(arr[i]);
+  }
+  return x % number === 0 ? true : false;
+}
