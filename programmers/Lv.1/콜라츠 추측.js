ea9f35a594fd88ede4541d0e949b61017c1bc633
Corete95 <fanthell95@gmail.com>
2022-04-12 23:50:34 +0900

    2022.04.12 Lv1 문제풀이

--- programmers/Lv.1/콜라츠 추측.js
+++ programmers/Lv.1/콜라츠 추측.js
@@ -21,7 +21,7 @@ function solution(num) {
   let answer = 0;
 
   for (let i = 0; i < 500; i++) {
-    if (num !== 1) {
+    if (num !== 1) {09
       if (num % 2 == 0) {
         num = num / 2;
       } else {
